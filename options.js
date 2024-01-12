7cb0169b787f594c4922c7828dfff893a2b4c5cd
Kai-Nalu <kainietogonzalez@gmail.com>
2024-01-12 13:17:13 -0500

    Added debug options

--- options.js
+++ options.js
@@ -8,7 +8,7 @@ const saveOptions = () => {
 			const status = document.getElementById('status');
 			status.textContent = 'Options saved.';
 			setTimeout(() => {
-				status.textContent = 'Options failed to save.';
+				status.textContent = '';
 			}, 750);
 		}
 	);
@@ -26,3 +26,31 @@ const restoreOptions = () => {
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click', saveOptions);
+
+const checkConnection = () => {
+	const connection_status = document.getElementById('connection-status');
+	connection_status.textContent = 'Testing...';
+	chrome.storage.sync.get(
+        { address: '', port: '' },
+        (items) => {
+            //make request url
+            const requestUrl = new URL(`http://${items.address}:${items.port}/status`);
+            //open a get request with fetch
+            fetch(requestUrl, {
+                signal: AbortSignal.timeout(8000)
+            }).then((response) => {
+                if (response.ok) {
+                    connection_status.textContent = 'Success!';
+                } else {
+                	connection_status.textContent = `Failed: ${response.status} | ${response.statusText}`;
+                }
+            }).catch((error) => {
+                if (error.name === "AbortError") {
+                    connection_status.textContent = 'Failed: timeout';
+                }
+            });
+        }
+    );
+}
+
+document.getElementById('check-connection').addEventListener('click', checkConnection);
